b8f941231f01ce5c3d5747cbcfd268cecd777826
NSchafy <njwschafhauser@gmail.com>
2025-05-03 06:16:23 -0400

    Pushing App controller class

---
+++ src/Gui/app.py
@@ -0,0 +1,16 @@
+from src.Model.Model import LikeModel
+from src.database.DatabaseHandler import DatabaseHandler
+from src.Controller.UserController import UserController
+from src.Controller.JobController import JobController
+
+class App:
+    def __init__(self):
+        self.db = DatabaseHandler(
+            host="localhost",
+            database="jobmatch",
+            user="your_user",
+            password="your_password"
+        )
+        self.model = LikeModel()
+        self.auth = UserController(self.db)
+        self.jobs = JobController(self.db, self.auth, self.model)
